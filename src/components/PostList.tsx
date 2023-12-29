adebf9b1c20e557df99a7de30268ae6410e0d174
champark <97137143+champark@users.noreply.github.com>
2023-12-30 00:38:05 +0900

    tab_control_completed

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,5 @@
 import AuthContext from "context/AuthContext";
-import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, orderBy, query, where } from "firebase/firestore";
 import { db } from "firebaseApp";
 import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -7,6 +7,7 @@ import { toast } from "react-toastify";
 
 interface PostListProps {
     hasNavigation?: boolean;
+    defaultTab?: TabType;
 }
 
 type TabType = "all" | "my";
@@ -22,27 +23,43 @@ export interface PostProps {
     uid: string;
 }
 
-export default function PostList({ hasNavigation = true }) {
-    const [activeTab, setActiveTab] = useState<TabType>("all");
+export default function PostList({ hasNavigation = true, 
+    defaultTab = "all", }:PostListProps) {
+    const [activeTab, setActiveTab] = useState<TabType>(defaultTab);
     const [posts, setPosts] = useState<PostProps[]>([]);
     const { user } = useContext(AuthContext);
 
     const getPosts = async () => {
-        const datas = await getDocs(collection(db, "posts"));
+        // const datas = await getDocs(collection(db, "posts"));
+        // posts 초기화
         setPosts([]);
+        let postsRef = collection(db, "posts");
+        let postsQuery;
 
+        if (activeTab === "my" && user) {
+            // 나의 글만 필터링
+            postsQuery = query(
+                postsRef, 
+                where("uid", "==", user.uid), 
+                orderBy("createdAt", "asc")
+                );
+        } else {
+            // 모든 글 보여주기
+            postsQuery = query(postsRef, orderBy("createdAt", "asc"));
+        }
+        const datas = await getDocs(postsQuery);
         datas?.forEach((doc) => {
-            console.log(doc.data(), doc.id);
+            // console.log(doc.data(), doc.id);
             const dataObj = { ...doc.data(), id: doc.id };
             setPosts((prev) => [...prev, dataObj as PostProps]);
         });
     };
 
-    const handleDelete = async(id: string) => {
+    const handleDelete = async (id: string) => {
         const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
         if (confirm && id) {
             await deleteDoc(doc(db, "posts", id));
-            
+
             toast.success('게시글을 삭제했습니다.');
             getPosts();     // 변경된 게시글 리스트를 다시 호출한다.
         }
@@ -50,7 +67,7 @@ export default function PostList({ hasNavigation = true }) {
 
     useEffect(() => {
         getPosts();
-    }, []);
+    }, [activeTab]);
 
     return (
         <>
